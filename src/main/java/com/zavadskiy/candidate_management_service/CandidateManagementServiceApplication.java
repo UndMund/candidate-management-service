0cb6613a667e49dbec10bb2cda5b82e4f09c990f
UndMund <zavadskiy.nazar@mail.ru>
2024-02-28 22:37:21 +0300

    Initial commit

---
+++ src/main/java/com/zavadskiy/candidate_management_service/CandidateManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zavadskiy.candidate_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidateManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zavadskiy/candidate_management_service/CandidateManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavadskiy.candidate_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
